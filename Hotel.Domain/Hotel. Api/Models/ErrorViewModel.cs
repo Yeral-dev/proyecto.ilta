70d50446a580766ecc677ddbaafe19c0c2c9185d
alexcordero0403@gmail.com <alexcordero0403@gmail.com>
2023-09-23 12:14:55 -0600

    Primero

---
+++ Hotel.Domain/Hotel. Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotel._Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
